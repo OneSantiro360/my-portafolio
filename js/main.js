e7121ffaae86f3267a2bcad6db199a9159e73472
OneSantiro360 <johndoe@example.com>
2022-07-02 21:56:43 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".menu-btn").classList.toggle("show");
+});
