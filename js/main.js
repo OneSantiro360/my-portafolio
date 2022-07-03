36804e34ff5aabbe099fd77c273e792de4c2c304
OneSantiro360 <johndoe@example.com>
2022-07-02 22:31:13 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".menu-btn").classList.toggle("show");
 });
+
+document.querySelector(".apartado").addEventListener("click", () => {
+    document.querySelector(".sub-menu").classList.toggle("show");
+});
+
+document.querySelector(".apartado2").addEventListener("click", () => {
+    document.querySelector(".sub-menu2").classList.toggle("show");
+});
